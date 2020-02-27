2bd9363a8f83affaa6a3218e10dcbc10290aaae3
Alan Rauzier <alan.rauzier@nestorparis.com>
2020-02-27 08:49:43 +0100

    Update dependencies to work with android sdk 29

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.1.7
+## 0.0.1
 
-* Update dependencies
+* TODO: Describe initial release.
--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/alanrauzier/nestor/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/alanrauzier/nestor/freshchat/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/archie/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/archie/AndroidStudioProjects/freshchat/example"
+export "FLUTTER_TARGET=/Users/archie/AndroidStudioProjects/freshchat/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/alanrauzier/nestor/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_FRAMEWORK_DIR=/Users/archie/development/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
